d40d6193198faee7359ccc275d14b7d15e59ac38
eagarer <erayagarer@gmail.com>
2023-08-21 15:21:51 +0300

    Added basic login page

--- App.js
+++ App.js
@@ -1,13 +1,43 @@
 import React from 'react';
-import {SafeAreaView, Text, View} from 'react-native';
+import {SafeAreaView, StyleSheet, TextInput, Button, Alert} from 'react-native';
 
-function App() {
+const UselessTextInput = () => {
+  const [username, onChangeUsername] = React.useState('');
+  const [password, onChangePassword] = React.useState('');
+
   return (
     <SafeAreaView>
-      <View>
-        <Text>Hello World</Text>
-      </View>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeUsername}
+        value={username}
+        placeholder="username"
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangePassword}
+        value={password}
+        placeholder="password"
+        secureTextEntry={true}
+      />
+      <Button
+        title="Log in"
+        color="#f194ff"
+        onPress={() =>
+          Alert.alert('Successfull' + ' ' + username + ' ' + password)
+        }
+      />
     </SafeAreaView>
   );
-}
-export default App;
+};
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+});
+
+export default UselessTextInput;
